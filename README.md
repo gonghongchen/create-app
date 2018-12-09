ea5689374b32fa524796b32c33e8ca142c3bad18
H&C <gonghongchen@23mofang.com>
2018-12-09 21:10:33 +0800

    添加styled-components

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 create-app 脚手架工程&学习工程
 
 基于CRA-ts
-Redux + Mobx + Router
+Redux + Mobx + Router + styled-components
+
+实现了 react 提供的按需加载功能
--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,12 +1,22 @@
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 import * as React from 'react';
+import styled from 'styled-components';
 
 class NotFound extends React.PureComponent<RouteComponentProps<any>> {
-    public render() {
-      return (
-        `404, Not found this url: ${this.props.location.pathname}`
-      )
-    }
+  public render() {
+    return (
+      <Span bgColor='#eee'>{`404, Not found this url: ${this.props.location.pathname}`}</Span>
+    )
   }
+}
+
+interface ISpanProps {
+  bgColor: string
+}
+
+const Span = styled.span`
+  color: red;
+  background-color: ${(props: ISpanProps) => props.bgColor}
+`
 
 export default withRouter(NotFound)
